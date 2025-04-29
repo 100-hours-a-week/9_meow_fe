f197332ab380e627ef9791bffa623405f4bf27be
jiyeeeah <sije3757@snu.ac.kr>
2025-04-29 13:19:47 +0900

    chore: 페이지 기반 라우팅 구현
    - Next.js로 마이그레이션 고려하여 페이지 기반 라우팅과
      비슷한 형태로 구현함
    - MVP에 필요한 페이지 5개만

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
-import App from "./app/App.tsx";
+import App from "./app/App";
+import CreatePostPage from "./app/create/CreatePostPage";
+import DetailPostPage from "./app/detail/[postId]/DetailPostPage";
+import LoginPage from "./app/login/LoginPage";
+import SignupPage from "./app/signup/SignupPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/create">게시글 작성</Link> |{" "}
+          <Link to="/detail/3">3번 게시물 상세보기</Link> |{" "}
+          <Link to="/login">로그인</Link> | <Link to="/signup">회원가입</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/create" element={<CreatePostPage />} />
+          <Route path="/detail/:postId" element={<DetailPostPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </Router>
   </StrictMode>
 );
