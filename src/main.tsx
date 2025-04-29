86e18e6cc877e1fa6669e88f907690fff4f2629b
jiyeeeah <sije3757@snu.ac.kr>
2025-04-29 17:53:06 +0900

    feat: NavigationBar 구현

--- src/main.tsx
+++ src/main.tsx
@@ -7,30 +7,15 @@ import CreatePostPage from "./app/create/CreatePostPage";
 import DetailPostPage from "./app/detail/[postId]/DetailPostPage";
 import LoginPage from "./app/login/LoginPage";
 import SignupPage from "./app/signup/SignupPage";
-import { Button } from "./components/ui/button";
-import { Header } from "./components/common";
+import { Header, NavigationBar } from "./components/common";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <div>
+      <div className="relative">
         <Header />
-        <nav>
-          <Link to="/">
-            <Button variant="primarySolid">Home</Button>
-          </Link>
-          <Link to="/create">
-            <Button variant="secondarySolid">게시글 작성</Button>
-          </Link>
-          <Link to="/detail/3">
-            <Button variant="primaryOutline">3번 게시물 상세보기</Button>
-          </Link>
-          <Link to="/login">
-            <Button variant="secondaryOutline">로그인</Button>
-          </Link>
-          <Link to="/signup">
-            <Button variant="link">회원가입</Button>
-          </Link>
-        </nav>
+        <NavigationBar />
+
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/create" element={<CreatePostPage />} />
