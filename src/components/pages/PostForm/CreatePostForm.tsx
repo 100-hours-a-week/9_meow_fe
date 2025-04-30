85ecd69ab4091bc28eb5cc0f0892ee13697a780b
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 15:28:05 +0900

    style: 게시글 작성 페이지 제목 추가

--- src/components/pages/PostForm/CreatePostForm.tsx
+++ src/components/pages/PostForm/CreatePostForm.tsx
@@ -2,7 +2,12 @@ import { ReactNode } from "react";
 import ImageInput from "./ImageInput";
 
 export default function CreatePostForm({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col gap-4 items-center pt-5">
+      <h1 className="text-3xl font-bold">오늘은 무슨 일이 있었냥</h1>
+      {children}
+    </div>
+  );
 }
 
 CreatePostForm.ImageInput = ImageInput;
