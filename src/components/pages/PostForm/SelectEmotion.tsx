0746934b5defcb75b557633a4c2e67a16e657602
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 16:39:00 +0900

    feat: SelectEmotion 컴포넌트 구현
    - radio group과 label 활용하여 구현
    - enum 활용하여 map으로 radio group 보여줌

---
+++ src/components/pages/PostForm/SelectEmotion.tsx
@@ -0,0 +1,27 @@
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { ApiEmotion, DisplayEmotion } from "@/types/Emotion";
+
+export default function SelectEmotion() {
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <p className="text-sm">오늘 기분은 어떠냥</p>
+      <RadioGroup
+        defaultValue={ApiEmotion.NONE}
+        className="flex flex-row gap-2 flex-wrap"
+      >
+        {Object.entries(ApiEmotion).map(([key, value], index) => (
+          <div key={value} className="flex items-center space-x-2">
+            <RadioGroupItem value={value} id={`r${index + 1}`} />
+            <Label htmlFor={`r${index + 1}`} className="text-xs">
+              {key === "NONE"
+                ? // TODO: 사용자에 따라서 다르게 보이도록
+                  DisplayEmotion.CAT
+                : DisplayEmotion[key as keyof typeof DisplayEmotion]}
+            </Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
+}
