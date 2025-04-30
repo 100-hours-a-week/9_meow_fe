7fdb2a07744bf0e80d8fd11982949ebb6889c469
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 09:10:21 +0900

    feat: 404 Not Found 페이지 구현
    - 존재하지 않는 URL로 접근시 404 페이지 띄움

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./app/App";
 import CreatePostPage from "./app/create/CreatePostPage";
 import DetailPostPage from "./app/detail/[postId]/DetailPostPage";
 import LoginPage from "./app/login/LoginPage";
 import SignupPage from "./app/signup/SignupPage";
+import NotFoundPage from "./app/not-found/NotFoundPage";
 import { Header, NavigationBar } from "./components/common";
 
 createRoot(document.getElementById("root")!).render(
@@ -22,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/detail/:postId" element={<DetailPostPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
