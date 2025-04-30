c3a4b116b7acc8b92a3b5a8e3eaa416ab81cdb6b
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 20:20:10 +0900

    feat: MainPage에서 게시글 목록 불러옴

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -1,44 +1,49 @@
-import { dummyPosts } from "@/assets/dummy-posts";
 import { PostCard } from "@/components/common";
 import { IPostContent } from "@/components/common/PostCard/PostContent";
 import { IUserItem } from "@/components/common/UserItem";
 import { IPostSummaryData } from "@/types/PostSummaryData";
 import { IPostFooter } from "@/components/common/PostCard/PostFooter";
+import usePostListInfiniteQuery from "@/hooks/queries/usePostListInfiniteQuery";
 
 export default function MainPage() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    usePostListInfiniteQuery();
+
   return (
     <div className="pt-2 pb-16 flex flex-col gap-2.5">
-      {dummyPosts.map((post: IPostSummaryData) => {
-        const userInfo: IUserItem = {
-          userId: post.userId,
-          nickname: post.nickname,
-          animalType: post.postType,
-          profileImageUrl:
-            post.profileImageUrl === "" ? undefined : post.profileImageUrl,
-        };
-        const postContent: IPostContent = {
-          postId: post.id,
-          thumbnailUrl:
-            post.thumbnailUrl === "" ? undefined : post.thumbnailUrl,
-          content: post.transformedContent,
-          animalType: post.postType,
-          timestamp: post.createdAt,
-          emotion: post.emotion,
-        };
-        const postInfo: IPostFooter = {
-          postId: post.id,
-          didLike: post.didLike,
-          likeCount: post.likeCount,
-          commentCount: post.commentCount,
-        };
-        return (
-          <PostCard key={`post-${post.id}`}>
-            <PostCard.Header userInfo={userInfo} />
-            <PostCard.Content {...postContent} />
-            <PostCard.Footer {...postInfo} />
-          </PostCard>
-        );
-      })}
+      {data?.pages.map((page) =>
+        page.content.map((post: IPostSummaryData) => {
+          const userInfo: IUserItem = {
+            userId: post.userId,
+            nickname: post.nickname,
+            animalType: post.postType,
+            profileImageUrl:
+              post.profileImageUrl === "" ? undefined : post.profileImageUrl,
+          };
+          const postContent: IPostContent = {
+            postId: post.id,
+            thumbnailUrl:
+              post.thumbnailUrl === "" ? undefined : post.thumbnailUrl,
+            content: post.transformedContent,
+            animalType: post.postType,
+            timestamp: post.createdAt,
+            emotion: post.emotion,
+          };
+          const postInfo: IPostFooter = {
+            postId: post.id,
+            didLike: post.didLike,
+            likeCount: post.likeCount,
+            commentCount: post.commentCount,
+          };
+          return (
+            <PostCard key={`post-${post.id}`}>
+              <PostCard.Header userInfo={userInfo} />
+              <PostCard.Content {...postContent} />
+              <PostCard.Footer {...postInfo} />
+            </PostCard>
+          );
+        })
+      )}
     </div>
   );
 }
