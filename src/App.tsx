e4b75e928138c82885c997e7cecd159448168660
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 19:54:03 +0900

    chore: Tanstack Query 사용하기 위해 QueryProvider
    - QueryProvider로 최상위 App.tsx에서 감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./app/MainPage";
 import CreatePostPage from "./app/create/CreatePostPage";
@@ -8,24 +9,28 @@ import NotFoundPage from "./app/not-found/NotFoundPage";
 import { Header, NavigationBar } from "./components/common";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div className="relative flex flex-col">
-        <Header />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/create" element={<CreatePostPage />} />
-            <Route path="/detail/:postId" element={<DetailPostPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="relative flex flex-col">
+          <Header />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/create" element={<CreatePostPage />} />
+              <Route path="/detail/:postId" element={<DetailPostPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+          <NavigationBar />
         </div>
-        <NavigationBar />
-      </div>
-    </Router>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
