4b71118b64a794113f90496d0fb6043562b2aabb
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 16:42:12 +0900

    style: 알림 개수 글자색 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       </div>
       <div className="relative">
         <img src="/icon/notification.svg" alt="notification" className="w-6" />
-        <div className="flex items-center justify-center absolute bg-destructive w-3 h-3 rounded-full -top-1 -right-1 text-xs">
+        <div className="flex items-center justify-center absolute bg-destructive w-3 h-3 rounded-full -top-1 -right-1 text-xs text-background/80">
           3
         </div>
       </div>
