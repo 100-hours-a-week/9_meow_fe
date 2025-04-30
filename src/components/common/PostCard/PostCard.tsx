928c0430a92dbb266a3d4cf97939ca4a26765ebb
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 10:47:01 +0900

    feat: PostFooter 간단 구현
    - 좋아요, 댓글, 공유 간단 구현
    - TODO : action 추가해줘야 함

--- src/components/common/PostCard/PostCard.tsx
+++ src/components/common/PostCard/PostCard.tsx
@@ -1,17 +1,18 @@
 import { IUserItem } from "../UserItem";
 import PostContent from "./PostContent";
-import PostFooter from "./PostFooter";
+import PostFooter, { IPostFooter } from "./PostFooter";
 import PostHeader from "./PostHeader";
 
 interface IPostCard {
   userInfo: IUserItem;
+  postInfo: IPostFooter;
 }
-export default function PostCard({ userInfo }: IPostCard) {
+export default function PostCard({ userInfo, postInfo }: IPostCard) {
   return (
     <div>
       <PostHeader userInfo={userInfo} />
       <PostContent />
-      <PostFooter />
+      <PostFooter {...postInfo} />
     </div>
   );
 }
