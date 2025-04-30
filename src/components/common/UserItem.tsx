a45e16bd87ce11c4cc53436ff75141821a4b3539
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 10:49:47 +0900

    refactor: profileImage -> profileImageUrl

--- src/components/common/UserItem.tsx
+++ src/components/common/UserItem.tsx
@@ -2,14 +2,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ApiAnimalType, convertAnimalTypeToDisplay } from "@/types/animal";
 export interface IUserItem {
   userId: number;
-  profileImage?: string;
+  profileImageUrl?: string;
   nickname: string;
   animalType: ApiAnimalType;
 }
 
 export default function UserItem({
   userId,
-  profileImage,
+  profileImageUrl,
   nickname,
   animalType,
 }: IUserItem) {
@@ -19,7 +19,9 @@ export default function UserItem({
   return (
     <div className="flex flex-row items-center gap-2" onClick={handleClick}>
       <Avatar>
-        <AvatarImage src={profileImage === "" ? "/logo.svg" : profileImage} />
+        <AvatarImage
+          src={profileImageUrl === "" ? "/logo.svg" : profileImageUrl}
+        />
         <AvatarFallback>미야옹</AvatarFallback>
       </Avatar>
       <div className="flex flex-col text-base">
