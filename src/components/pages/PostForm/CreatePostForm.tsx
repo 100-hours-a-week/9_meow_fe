52dbbfabad67a9ac6f2f29db78e03fe3aaf778ea
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 15:41:51 +0900

    feat: PostContentInput 구현
    - 게시글의 내용을 입력할 수 있는 textarea를 구현
    - 200자 초과하는 경우 helpertext 붉은색 되도록

--- src/components/pages/PostForm/CreatePostForm.tsx
+++ src/components/pages/PostForm/CreatePostForm.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react";
 import ImageInput from "./ImageInput";
+import PostContentInput from "./PostContentInput";
 
 export default function CreatePostForm({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col gap-4 items-center pt-5">
+    <div className="flex flex-col gap-4 items-center p-5">
       <h1 className="text-3xl font-bold">오늘은 무슨 일이 있었냥</h1>
       {children}
     </div>
@@ -11,3 +12,4 @@ export default function CreatePostForm({ children }: { children: ReactNode }) {
 }
 
 CreatePostForm.ImageInput = ImageInput;
+CreatePostForm.PostContentInput = PostContentInput;
