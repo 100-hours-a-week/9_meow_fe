7f39e0891379a344b4a4bb74637191b6ac2f9880
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 16:45:59 +0900

    feat: CreatePostForm에 버튼 추가
    - 취소 버튼, 게시 버튼 추가

--- src/components/pages/PostForm/CreatePostForm.tsx
+++ src/components/pages/PostForm/CreatePostForm.tsx
@@ -2,12 +2,17 @@ import { ReactNode } from "react";
 import ImageInput from "./ImageInput";
 import PostContentInput from "./PostContentInput";
 import SelectEmotion from "./SelectEmotion";
+import { Button } from "@/components/ui/button";
 
 export default function CreatePostForm({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col gap-4 items-center p-5">
       <h1 className="text-3xl font-bold">오늘은 무슨 일이 있었냥</h1>
       {children}
+      <div className="flex gap-2 w-full justify-end">
+        <Button variant="primarySolid">취소냥</Button>
+        <Button variant="secondarySolid">£ 다 적으면 누르라냥!</Button>
+      </div>
     </div>
   );
 }
