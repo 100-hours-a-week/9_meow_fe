12d9282350376c343df30b9f7a3c41f9e10988ed
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 19:52:33 +0900

    refactor: Router설정 App.tsx로 이동
    - Router 설정은 main.tsx보다 최상위 App.tsx에 있는게 자연스러움
    - App.tsx가 애플리케이션의 주요 구조와 설정을 담당하는 컴포넌트이기 때움
    - 기존의 App.tsx는 MainPage.tsx로 수정함

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./app/App";
-import CreatePostPage from "./app/create/CreatePostPage";
-import DetailPostPage from "./app/detail/[postId]/DetailPostPage";
-import LoginPage from "./app/login/LoginPage";
-import SignupPage from "./app/signup/SignupPage";
-import NotFoundPage from "./app/not-found/NotFoundPage";
-import { Header, NavigationBar } from "./components/common";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router>
-      <div className="relative flex flex-col">
-        <Header />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/create" element={<CreatePostPage />} />
-            <Route path="/detail/:postId" element={<DetailPostPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-        <NavigationBar />
-      </div>
-    </Router>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
