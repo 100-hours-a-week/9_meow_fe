a6fe6283a563ce3f4ee5d429370e23a968c289ca
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 09:05:31 +0900

    refactor: NavigationBar 내의 불필요한 함수 제거
    - renderProfileImage는 Avatar 컴포넌트로 대체할 수 있음
    - 한번만 사용되기 때문에 제거하는 것이 맞다고 판단함

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,5 +1,6 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function renderIconButton(route: string, icon: React.ReactNode) {
@@ -10,25 +11,23 @@ function renderIconButton(route: string, icon: React.ReactNode) {
   );
 }
 
-function renderProfileImage(src?: string) {
-  if (!src) {
-    return (
-      <Avatar>
-        <AvatarImage src="/logo.svg" />
-      </Avatar>
-    );
-  }
-  return <img src={src} alt="profile" />;
-}
-
 export default function NavigationBar() {
+  // TODO : 프로필 이미지 추가
+  const [profileImage] = useState<string | undefined>();
+
   return (
     <div className="flex justify-between items-center border-t border-border/30 px-5 py-1 fixed bottom-0 w-full bg-background">
       {renderIconButton("/", <img src="/icon/home.svg" alt="home" />)}
       {/* {renderIconButton("/icon/calendar.svg", "calendar", "/event")} */}
       {renderIconButton("/create", <img src="/icon/plus.svg" alt="plus" />)}
       {/* {renderIconButton("/icon/chat.svg", "chat", "/chat")} */}
-      {renderIconButton("/member", renderProfileImage())}
+      {renderIconButton(
+        "/member",
+        <Avatar>
+          <AvatarImage src={profileImage ?? "/logo.svg"} />
+          <AvatarFallback>미야옹</AvatarFallback>
+        </Avatar>
+      )}
     </div>
   );
 }
