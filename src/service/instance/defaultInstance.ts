8bc6b89a6a64cc4f14b003813bc7467c12cbab49
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 20:02:11 +0900

    chore: axios instance 만듦
    - default instance와 form instance로 나눔

---
+++ src/service/instance/defaultInstance.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const baseURL = `${import.meta.env.VITE_API_URL}`;
+
+const defaultInstance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export default defaultInstance;
