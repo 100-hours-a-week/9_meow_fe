04401a9c832398fbf1761655b025abb34704ae6e
jiyeeeah <sije3757@snu.ac.kr>
2025-04-30 09:54:28 +0900

    feat: PostCard 골격 세팅, PostHeader 구현
    - PostCard에 필요한 PostHeader, PostFooter, PostContent
      파일 만들어서 예시 작성
    - 그 중 PostHeader는 UserItem 활용해서 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
+import { PostCard } from "@/components/common";
+import { IUserItem } from "@/components/common/UserItem";
+
 function App() {
+  const userInfo: IUserItem = {
+    userId: 1,
+    nickname: "홍길동",
+    animalType: "고양이",
+  };
+
   return (
     <div className="min-h-screen pb-16">
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <PostCard userInfo={userInfo} />
     </div>
   );
 }
--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,3 +1,4 @@
 export { default as UserItem } from "./UserItem";
 export { default as Header } from "./Header";
 export { default as NavigationBar } from "./NavigationBar";
+export { default as PostCard } from "./PostCard/PostCard";
