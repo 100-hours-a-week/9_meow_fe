c96de2877633e12eb4d449a18b5c152f26e3634a
jiyeeeah <sije3757@snu.ac.kr>
2025-05-01 11:49:58 +0900

    feat: 게시글 상세보기 페이지 UI 구현

--- src/assets/dummy-post-detail.ts
+++ src/assets/dummy-post-detail.ts
@@ -7,7 +7,7 @@ export const dummyPostDetail: IPostDetailData = {
   userId: 1,
   nickname: "TestUser",
   profileImageUrl: "",
-  transformedContent: "This is a dummy post",
+  transformedContent: `흠, 오늘 날씨? 나쁘지 않군. 내가 산책 나가주기엔 딱 좋은 정도지. \n☀️ 밖에 나가면 또 사람들이 나 보느라 정신 못 차리겠지, 뭐. 이번 주 벚꽃이 예쁘다던데… 그래, 꽃도 나만큼은 아니겠지만 꽤 볼만할지도. 하지만 벚꽃보다 내가 더 예쁘다는 건 부정 못 하겠지? \n🌸✨ 나 없인 봄도 심심할 거다옹.`,
   emotion: ApiEmotion.HAPPY,
   postType: ApiAnimalType.CAT,
   imageUrls: [
--- src/components/common/PostCard/PostHeader.tsx
+++ src/components/common/PostCard/PostHeader.tsx
@@ -6,7 +6,7 @@ interface IPostHeader {
 
 export default function PostHeader({ userInfo }: IPostHeader) {
   return (
-    <div className="flex flex-row items-center justify-between">
+    <div className="flex flex-row items-center justify-between w-full">
       <UserItem {...userInfo} />
       {/* TODO : 사용자의 게시글인지에 따라 옵션 아이콘, context menu 추가 */}
       {/* <img src="/icon/dots-vertical.svg" alt="options" /> */}
