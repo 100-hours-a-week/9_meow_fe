5059c4d9936c95d574ac743cdcf714df943711be
jiyeeeah <sije3757@snu.ac.kr>
2025-05-07 13:23:03 +0900

    feat: 게시글 작성 pending, success, error 처리 추가
    - 게시글 작성 요청을 보냈을 때, pending 중이라면 button을 disable
    - success 라면 메인 화면으로 이동 시킴
    - error라면 alert 띄움

--- src/components/pages/PostForm/CreatePostForm.tsx
+++ src/components/pages/PostForm/CreatePostForm.tsx
@@ -1,14 +1,25 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import ImageInput from "./ImageInput";
 import PostContentInput from "./PostContentInput";
 import SelectEmotion from "./SelectEmotion";
 import { Button } from "@/components/ui/button";
 import usePostMutation from "@/hooks/mutations/usePostMutation";
 import useCreatePostStore from "@/store/useCreatePostStore";
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePostForm({ children }: { children: ReactNode }) {
-  const { mutate: postPost } = usePostMutation();
+  const navigate = useNavigate();
+  const { mutate: postPost, isPending, isSuccess, isError } = usePostMutation();
   const { selectedImages, content } = useCreatePostStore();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/");
+    }
+    if (isError) {
+      alert("게시글 작성에 실패했어요. 다시 시도해주세요.");
+    }
+  }, [isSuccess, isError, navigate]);
 
   return (
     <div className="flex flex-col gap-4 items-center p-5">
@@ -18,11 +29,12 @@ export default function CreatePostForm({ children }: { children: ReactNode }) {
         <Button variant="primarySolid">취소냥</Button>
         <Button
           variant="secondarySolid"
+          disabled={isPending}
           onClick={() =>
             postPost({ images: selectedImages.map((img) => img.file), content })
           }
         >
-          £ 다 적으면 누르라냥!
+          {isPending ? "잠시만 기다려주세요냥" : "다 적으면 누르라냥!"}
         </Button>
       </div>
     </div>
