8b44bac844295c99dd7cbe1a8cf0cafe29c00c21
jiyeeeah <sije3757@snu.ac.kr>
2025-05-07 18:21:45 +0900

    feat: 프로필 사진 선택하는 컴포넌트 구현

--- src/components/pages/MemberForm/ProfileImageSelection.tsx
+++ src/components/pages/MemberForm/ProfileImageSelection.tsx
@@ -1,3 +1,50 @@
-export default function ProfileImageSelection() {
-  return <div>ProfileImageSelection</div>;
+import { ChangeEvent } from "react";
+import { useImagePreview } from "@/hooks/common/useImagePreview";
+
+interface IProfileImageSelection {
+  selectedImage: File | null;
+  setSelectedImage: (image: File) => void;
+}
+
+export default function ProfileImageSelection({
+  selectedImage,
+  setSelectedImage,
+}: IProfileImageSelection) {
+  const { previewUrl, createPreview } = useImagePreview({
+    initialImage: selectedImage,
+  });
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedImage(file);
+      createPreview(file);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-0 items-center px-2 py-2">
+      <div className="flex gap-3">
+        <label className="flex items-center justify-center w-[100px] h-[100px] bg-orange-950 border border-foreground/30 rounded-full cursor-pointer overflow-hidden">
+          {previewUrl ? (
+            <img
+              src={previewUrl}
+              alt="Profile preview"
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="flex flex-col items-center gap-2">
+              <span className="text-2xl text-background">+</span>
+            </div>
+          )}
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleImageChange}
+          />
+        </label>
+      </div>
+    </div>
+  );
 }
