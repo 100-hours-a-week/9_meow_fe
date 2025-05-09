589ec1530bcdbf5146eed24c2792608329f0ef08
jiyeeeah <sije3757@snu.ac.kr>
2025-05-09 18:15:51 +0900

    style: 메인 페이지 마진 추가
    - 메인 페이지의 양쪽 마진 추가
    - PostCard 에 그림자 추가

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -37,7 +37,7 @@ export default function MainPage() {
   }
 
   return (
-    <div className="pt-2 pb-16 flex flex-col gap-2.5">
+    <div className="pt-2 pb-16 flex flex-col gap-2.5 px-2">
       {data.pages.map((page) =>
         page.content.map((post: IPostSummaryData) => {
           const userInfo: IUserItem = {
