f1cf17aa94905f4cf00271d5da71a2e8964832b2
jiyeeeah <sije3757@snu.ac.kr>
2025-05-10 15:12:52 +0900

    feat: 리다이렉트 시에 이미 회원이면 로그인 되도록

--- src/app/redirect/RedirectPage.tsx
+++ src/app/redirect/RedirectPage.tsx
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 import useKakaoIdStore from "@/store/useKakaoIdStore";
 import { useMutation } from "@tanstack/react-query";
 import { ILoginCode, IKakaoAuthResponse } from "@/types/Login";
-import { getKakaoId } from "@/service/login";
+import { getKakaoId, postLogin } from "@/service/login";
+import useTokenStore from "@/store/useTokenStore";
 
 export default function RedirectPage() {
   const navigate = useNavigate();
   const setKakaoId = useKakaoIdStore((state) => state.setKakaoId);
+  const setToken = useTokenStore((state) => state.setToken);
 
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
@@ -26,17 +28,30 @@ export default function RedirectPage() {
     },
   });
 
+  const { mutate: login } = useMutation({
+    mutationFn: (kakaoId: number) => postLogin(kakaoId),
+    onSuccess: (data) => {
+      setToken(data.token);
+      navigate("/");
+    },
+    onError: (error) => {
+      console.error("Login failed:", error);
+      alert("로그인에 실패했다옹... 다시 시도해달라옹");
+    },
+  });
+
   useEffect(() => {
     if (code) {
       getKakao({ code });
 
       if (data?.isMember) {
         navigate("/");
+        login(data.kakaoId);
       } else {
         navigate("/signup");
       }
     }
-  }, [code, getKakao, data, navigate, setKakaoId]);
+  }, [code, getKakao, data, navigate, setKakaoId, login]);
 
   return <div>♧ 리디렉션 중이다옹...</div>;
 }
