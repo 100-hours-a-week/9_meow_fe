1cfba5190b4aef749e5b65d67421ed5353f33800
kimhyungjin1125 <kimpeanut77@gmail.com>
2025-05-13 17:24:50 +0900

    Create FE CI

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,45 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  server: {
+  port: 4173,
+  proxy: {
+    "/api/infer": {
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    },
+    "/api": {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    },
+  },
+},
+preview: {
+  port: 4173,
+  proxy: {
+    "/api/infer": {
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    },
+    "/api": {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    },
+  },
+},
+  // CI 빌드 아웃풋 설정
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
