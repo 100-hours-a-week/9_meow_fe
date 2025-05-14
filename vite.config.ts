8c30245bf1a82106f334a97e53f947f304572aea
jiyeeeah <sije3757@snu.ac.kr>
2025-05-14 15:10:58 +0900

    fix: port 번호 수정
    - 5173 인데 4173으로 되어있던 port 번호
      config 파일에서 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,31 +13,31 @@ export default defineConfig({
   },
 
   server: {
-  port: 4173,
-  proxy: {
-    "/api/infer": {
-      target: "http://localhost:8000",
-      changeOrigin: true,
-    },
-    "/api": {
-      target: "http://localhost:8080",
-      changeOrigin: true,
-    },
-  },
-},
-preview: {
-  port: 4173,
-  proxy: {
-    "/api/infer": {
-      target: "http://localhost:8000",
-      changeOrigin: true,
-    },
-    "/api": {
-      target: "http://localhost:8080",
-      changeOrigin: true,
+    port: 5173,
+    proxy: {
+      "/api/infer": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
     },
   },
-},
+  preview: {
+    port: 5173,
+    proxy: {
+      "/api/infer": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   // CI 빌드 아웃풋 설정
   build: {
     outDir: "dist",
