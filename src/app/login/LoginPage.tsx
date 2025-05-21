3851c841d4cfc2bdabc097fcac1cb3bc858f96b7
jiyeeeah <sije3757@snu.ac.kr>
2025-05-21 11:49:30 +0900

    feat: 로그아웃 버튼 만듦
    - 로그인 페이지에 로그아웃 버튼 만들어서 local storage에 있는
      토큰 지우는 기능 추가

--- src/app/login/LoginPage.tsx
+++ src/app/login/LoginPage.tsx
@@ -1,23 +1,11 @@
-import { LoginButton } from "@/components/pages";
-import { TOKEN_KEY } from "@/store/useTokenStore";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { LoginButton, LogoutButton } from "@/components/pages";
 
 export default function LoginPage() {
-  const navigate = useNavigate();
-  const token = localStorage.getItem(TOKEN_KEY);
-
-  useEffect(() => {
-    if (token) {
-      alert("이미 로그인 되어 있다옹");
-      navigate(-1);
-    }
-  }, [navigate, token]);
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh] gap-10">
-      <h2 className="text-4xl">£ 어서오라옹 ¢</h2>
+    <div className="flex flex-col items-center justify-center min-h-[60vh] gap-5">
+      <h2 className="text-3xl">£ 어서오라옹 ¢</h2>
       <LoginButton />
+      <LogoutButton />
     </div>
   );
 }
