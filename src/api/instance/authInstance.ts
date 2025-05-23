a4e023b07735147046c0c3b2ea587a195560466a
jiyeeeah <sije3757@snu.ac.kr>
2025-05-23 15:38:28 +0900

    refactor: auth 필요한 instance에 token refresh하는 로직 추가
    - auth가 필요한 instance (authInstance, formInstance)에 401로 요청
      실패시 refresh 요청으로 새로운 access token 발급 받는 로직 추가
    - 새로운 access token으로 해당 요청 재시도

--- src/api/instance/authInstance.ts
+++ src/api/instance/authInstance.ts
@@ -1,5 +1,6 @@
-import { TOKEN_KEY } from "@/store/useTokenStore";
+import useTokenStore, { TOKEN_KEY } from "@/store/useTokenStore";
 import axios from "axios";
+import { postRefresh } from "../login";
 
 const baseURL = `${import.meta.env.VITE_API_URL}`;
 
@@ -19,6 +20,30 @@ authInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+authInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const response: { accessToken: string } = await postRefresh();
+        const newToken = response.accessToken;
+        useTokenStore.getState().setToken(newToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return authInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
--- src/api/instance/formInstance.ts
+++ src/api/instance/formInstance.ts
@@ -1,5 +1,6 @@
-import { TOKEN_KEY } from "@/store/useTokenStore";
 import axios from "axios";
+import { postRefresh } from "../login";
+import useTokenStore from "@/store/useTokenStore";
 
 const baseURL = `${import.meta.env.VITE_API_URL}`;
 
@@ -12,13 +13,37 @@ const formInstance = axios.create({
 
 formInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem(TOKEN_KEY);
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+formInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const response: { accessToken: string } = await postRefresh();
+        const newToken = response.accessToken;
+        useTokenStore.getState().setToken(newToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return formInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
