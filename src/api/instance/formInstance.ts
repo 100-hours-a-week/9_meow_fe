480d778b8814afd02170c5a3be8edb469b09b47a
jiyeeeah <sije3757@snu.ac.kr>
2025-05-23 15:56:12 +0900

    fix: token key 상수 사용하도록
    - formInstance에서 token은 "token"으로 가져오고 있었음
      -> token key 상수로 지정했던 것으로 수정해줌

--- src/api/instance/formInstance.ts
+++ src/api/instance/formInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { postRefresh } from "../login";
-import useTokenStore from "@/store/useTokenStore";
+import useTokenStore, { TOKEN_KEY } from "@/store/useTokenStore";
 
 const baseURL = `${import.meta.env.VITE_API_URL}`;
 
@@ -13,7 +13,7 @@ const formInstance = axios.create({
 
 formInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem(TOKEN_KEY);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
