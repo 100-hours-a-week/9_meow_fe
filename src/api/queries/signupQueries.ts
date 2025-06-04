d2e283f86fdd8e9d37d11657c75ba52d11c6d30a
jiyeeeah <sije3757@snu.ac.kr>
2025-06-04 16:27:44 +0900

    refactor: checkNickname 부분 리팩토링
    - useEffect로 Error와 Success 처리
      -> onSuccess와 onError에서 처리하도록 수정

--- src/api/queries/signupQueries.ts
+++ src/api/queries/signupQueries.ts
@@ -5,9 +5,27 @@ import { IUserRequest } from "@/api/types";
 export const signupQueries = {
   all: () => ["signup"] as const,
 
-  checkNickname: (): UseMutationOptions<boolean, Error, string> => ({
+  checkNickname: ({
+    setIsNicknameDuplicate,
+    setErrorMessage,
+    setSuccessMessage,
+  }: {
+    setIsNicknameDuplicate?: (isDuplicate: boolean) => void;
+    setErrorMessage: (error: string | null) => void;
+    setSuccessMessage: (success: string | null) => void;
+  }): UseMutationOptions<boolean, Error, string> => ({
     mutationKey: [...signupQueries.all(), "checkNickname"],
     mutationFn: (nickname: string) => getDuplicateNickname(nickname),
+    onSuccess: (data: boolean) => {
+      setIsNicknameDuplicate?.(data);
+      setErrorMessage(data ? "중복된 닉네임이 있다옹" : null);
+      setSuccessMessage(data ? null : "사용 가능한 닉네임이다옹!");
+    },
+    onError: () => {
+      setIsNicknameDuplicate?.(true);
+      setErrorMessage("문제 생겼다옹... 잠시 후 다시 시도해라옹");
+      setSuccessMessage(null);
+    },
   }),
 
   signup: ({
