cfb4f0fc69e1adf833ef65b449306f0a3be2a7d4
jiyeeeah <sije3757@snu.ac.kr>
2025-06-09 16:31:14 +0900

    feat: useImageUpload 훅에서 S3에 업로드하는 로직 추가
    - useImageUpload에 uploadImagesToS3라는 함수 추가하여,
      selectedImages를 모두 S3에 업로드하도록

--- src/hooks/common/useImageUpload.ts
+++ src/hooks/common/useImageUpload.ts
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { imageQueries } from "@/api/queries/ImageQueries";
 
-export interface PreviewImage {
+export interface IPreviewImage {
   file: File;
   preview: string;
 }
 
 export const useImageUpload = () => {
-  const [selectedImages, setSelectedImages] = useState<PreviewImage[]>([]);
+  const [selectedImages, setSelectedImages] = useState<IPreviewImage[]>([]);
+
+  const { mutateAsync: uploadImageToS3, isPending: isUploading } = useMutation({
+    ...imageQueries.uploadImageToS3(),
+  });
 
   const addImage = async (file: File) => {
-    const newImage = await new Promise<PreviewImage>((resolve) => {
+    const newImage = await new Promise<IPreviewImage>((resolve) => {
       const reader = new FileReader();
       reader.onload = (e: ProgressEvent<FileReader>) => {
         const result = e.target?.result;
@@ -31,9 +37,31 @@ export const useImageUpload = () => {
     });
   };
 
+  const uploadImagesToS3 = async (): Promise<string[]> => {
+    if (selectedImages.length === 0) return [];
+
+    try {
+      const imageUrls = await Promise.all(
+        selectedImages.map(async (image) => {
+          const key = await uploadImageToS3(image.file);
+
+          const cloudFrontUrl = import.meta.env.VITE_CLOUDFRONT_URL;
+          return `${cloudFrontUrl}${key}`;
+        }),
+      );
+
+      return imageUrls;
+    } catch (error) {
+      console.error("이미지 업로드 중 오류 발생:", error);
+      throw new Error("이미지 업로드에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
   return {
     selectedImages,
     addImage,
     removeImage,
+    isUploading,
+    uploadImagesToS3,
   };
 };
