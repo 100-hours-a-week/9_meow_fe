5066112541714bf7e012a5fb0fc459e36a4c6f60
jiyeeeah <sije3757@snu.ac.kr>
2025-06-11 14:23:19 +0900

    refactor: MyProfileSummary 제거
    - MyProfileSummary는 불필요해졌으므로 제거

--- src/components/pages/MemberPage/ProfileSummary.tsx
+++ src/components/pages/MemberPage/ProfileSummary.tsx
@@ -2,26 +2,41 @@ import { Button } from "@/components/ui/button";
 import MemberInfoSummary from "./ProfileSummary/MemberInfoSummary";
 import { ApiAnimalType } from "@/types/animal";
 import ProfileInfo from "./ProfileSummary/ProfileInfo";
+import { useNavigate } from "react-router-dom";
 
 interface IProfileSummary {
   userId: number;
 }
 
 export default function ProfileSummary({ userId }: IProfileSummary) {
+  const navigate = useNavigate();
+
   // TODO: 멤버 정보 조회 (지금은 dummy data)
   console.log(userId);
   const memberInfo = {
     profileImageUrl: "/logo.svg",
     nickname: "미야옹",
     animalType: ApiAnimalType.CAT,
+    currentUser: false,
   };
+
   return (
     <div className="w-full flex flex-col gap-4 items-center">
       <MemberInfoSummary {...memberInfo} />
       <div className="flex flex-row gap-5">
-        <Button variant="primaryOutline" className="w-36 text-lg">
-          팔로우
-        </Button>
+        {memberInfo.currentUser ? (
+          <Button
+            variant="primarySolid"
+            className="w-36 text-lg"
+            onClick={() => navigate("/mypage/edit")}
+          >
+            프로필 편집
+          </Button>
+        ) : (
+          <Button variant="primaryOutline" className="w-36 text-lg">
+            팔로우
+          </Button>
+        )}
         <Button variant="primarySolid" className="w-36 text-lg">
           프로필 공유
         </Button>
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -8,5 +8,4 @@ export { default as LogoutButton } from "./Login/LogoutButton";
 export { default as CommentItem } from "./Comment/CommentItem";
 export { default as CommentInput } from "./Comment/CommentInput";
 export { default as ProfileSummary } from "./MemberPage/ProfileSummary";
-export { default as MyProfileSummary } from "./MemberPage/MyProfileSummary";
 export { default as MemberItem } from "./MemberPage/MemberItem";
