06344238d0e1decc6342f082d9416712b3d851a5
jiyeeeah <sije3757@snu.ac.kr>
2025-06-11 16:42:50 +0900

    style: ProfileInfo 버튼들 스타일 수정
    - ProfileInfo에 있는 팔로워/팔로잉 div -> Button으로 수정

--- src/components/pages/MemberPage/ProfileSummary/ProfileInfo.tsx
+++ src/components/pages/MemberPage/ProfileSummary/ProfileInfo.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 interface IProfileInfo {
   postCount: number;
   followerCount: number;
@@ -10,19 +12,28 @@ export default function ProfileInfo({
   followingCount,
 }: IProfileInfo) {
   return (
-    <div className="w-full flex flex-row gap-12 justify-center border-t border-b border-foreground/30 py-3">
-      <div className="flex flex-col items-center">
+    <div className="w-full flex flex-row gap-10 justify-center items-center border-t border-b border-foreground/30 py-1">
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0 w-16 h-full"
+      >
         <p className="text-base">{postCount}</p>
         <p className="text-xs text-foreground/50">Post</p>
-      </div>
-      <div className="flex flex-col items-center">
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0 w-16 h-full"
+      >
         <p className="text-base">{followerCount}</p>
         <p className="text-xs text-foreground/50">Follower</p>
-      </div>
-      <div className="flex flex-col items-center">
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0 w-16 h-full"
+      >
         <p className="text-base">{followingCount}</p>
         <p className="text-xs text-foreground/50">Following</p>
-      </div>
+      </Button>
     </div>
   );
 }
