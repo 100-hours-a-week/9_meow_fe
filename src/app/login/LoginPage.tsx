49d97016e2d32e768a11d878f09cac3773205452
jiyeeeah <sije3757@snu.ac.kr>
2025-06-16 14:17:11 +0900

    fix: 로그인 여부에 따라 로그아웃 버튼
    - LoginPage
    - 로그인 되어있으면 로그아웃 버튼만 보이고
      로그인 안 되어 있으면 로그인 버튼만 보임

--- src/app/login/LoginPage.tsx
+++ src/app/login/LoginPage.tsx
@@ -1,11 +1,13 @@
 import { LoginButton, LogoutButton } from "@/components/pages";
+import useTokenStore from "@/store/useTokenStore";
 
 export default function LoginPage() {
+  const { token } = useTokenStore();
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] gap-5">
       <h2 className="text-3xl">£ 어서오라옹 ¢</h2>
-      <LoginButton />
-      <LogoutButton />
+      {!token && <LoginButton />}
+      {token && <LogoutButton />}
     </div>
   );
 }
