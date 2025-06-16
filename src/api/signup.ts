143d3ef3d3cee31d0d0c93a9abccd8990b330098
jiyeeeah <sije3757@snu.ac.kr>
2025-06-16 15:03:36 +0900

    fix: 회원가입 defaultInstance 타도록
    - 이미지 업로드 방식 변경 된 후로 File을 전송할 필요 없음
    - json으로 전송하도록 defaultInstance 사용하게 수정함

--- src/api/signup.ts
+++ src/api/signup.ts
@@ -1,23 +1,13 @@
-import formInstance from "./instance/formInstance";
+import defaultInstance from "./instance/defaultInstance";
 import { ISignupUserRequest } from "./types/signup";
 
 export const postUsers = async (user: ISignupUserRequest) => {
-  const formData = new FormData();
-
-  if (user.profileImage) {
-    formData.append(`profileImage`, user.profileImage);
-  }
-
-  formData.append("kakaoId", user.kakaoId.toString());
-  formData.append("nickname", user.nickname);
-  formData.append("animalType", user.animalType);
-
-  const response = await formInstance.post("/users", formData);
+  const response = await defaultInstance.post("/users", user);
   return response.data;
 };
 
 export const getDuplicateNickname = async (nickname: string) => {
-  const response = await formInstance.get<boolean>(
+  const response = await defaultInstance.get<boolean>(
     `/users/check-nickname?nickname=${nickname}`,
   );
   return response.data;
