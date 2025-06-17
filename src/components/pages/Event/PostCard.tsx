1cb0279821c53fa7cbbea5a1d728695b820aed89
jiyeeeah <sije3757@snu.ac.kr>
2025-06-17 17:23:54 +0900

    feat: EventPostCard에 선택적으로 뱃지 보여줌
    - rank가 있는 경우 (none이 아닌 경우) 진, 선, 미 뱃지 붙여줌

--- src/components/pages/Event/PostCard.tsx
+++ src/components/pages/Event/PostCard.tsx
@@ -1,13 +1,29 @@
 import { IUserItem } from "@/components/common/UserItem";
 import EventPostFooter from "./PostFooter";
 
+type TRank = "1st" | "2nd" | "3rd" | "none";
+
 interface IEventPostCard {
   postId: number;
   postImageUrl: string;
   userInfo: IUserItem;
   likeCount: number;
-  rank?: "1st" | "2nd" | "3rd" | "none";
+  rank?: TRank;
 }
+
+function renderBadge(rank: TRank) {
+  switch (rank) {
+    case "1st":
+      return <img src={"/icon/badge/first-badge.PNG"} alt="badge" />;
+    case "2nd":
+      return <img src={"/icon/badge/second-badge.PNG"} alt="badge" />;
+    case "3rd":
+      return <img src={"/icon/badge/third-badge.PNG"} alt="badge" />;
+    default:
+      return null;
+  }
+}
+
 export default function EventPostCard({
   postId,
   postImageUrl,
@@ -16,7 +32,8 @@ export default function EventPostCard({
   rank = "none",
 }: IEventPostCard) {
   return (
-    <div className="w-[100px] flex flex-col gap-1">
+    <div className="relative w-[100px] flex flex-col gap-1">
+      <div className="absolute top-1 left-1 w-[20px]">{renderBadge(rank)}</div>
       <div className="w-full h-[100px] rounded-lg overflow-hidden border border-muted-foreground">
         <img
           src={postImageUrl}
@@ -28,7 +45,6 @@ export default function EventPostCard({
         postId={postId}
         userInfo={userInfo}
         likeCount={likeCount}
-        rank={rank}
       />
     </div>
   );
--- src/components/pages/Event/PostFooter.tsx
+++ src/components/pages/Event/PostFooter.tsx
@@ -6,29 +6,27 @@ interface IEventPostFooter {
   postId: number;
   userInfo: IUserItem;
   likeCount: number;
-  rank?: "1st" | "2nd" | "3rd" | "none";
 }
+
 export default function EventPostFooter({
   postId,
   userInfo,
   likeCount,
-  rank = "none",
 }: IEventPostFooter) {
   return (
     <div className="flex flex-row justify-between">
       <UserItem {...userInfo} size="sm" />
-      <div className="flex flex-row gap-1 items-center">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => {
-            console.log("postId: ", postId);
-          }}
-        >
-          <img src="/icon/paw.svg" alt="paw" />
-        </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="flex flex-row gap-1 items-center"
+        onClick={() => {
+          console.log("postId: ", postId);
+        }}
+      >
+        <img src="/icon/paw.svg" alt="paw" />
         <p className="text-foreground text-xs">{likeCount}</p>
-      </div>
+      </Button>
     </div>
   );
 }
