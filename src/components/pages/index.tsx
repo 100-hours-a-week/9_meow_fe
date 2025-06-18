588fd0cfe8535fd65aae3a8dc4d2af8457dd6a4a
jiyeeeah <sije3757@snu.ac.kr>
2025-06-18 16:17:09 +0900

    feat: EventTop3 컴포넌트 구현

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -11,3 +11,4 @@ export { default as ProfileSummary } from "./MemberPage/ProfileSummary";
 export { default as MemberItem } from "./MemberPage/MemberItem";
 export { default as EventPostCard } from "./Event/PostCard";
 export { default as EventHistoryCard } from "./Event/HistoryCard";
+export { default as EventTop3 } from "./Event/EventTop3";
