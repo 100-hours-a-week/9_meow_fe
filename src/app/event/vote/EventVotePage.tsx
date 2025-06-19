3acf682c8aae95f8b5b730f11ccbc00a8aa80e21
jiyeeeah <sije3757@snu.ac.kr>
2025-06-19 16:25:28 +0900

    feat: 투표 페이지에서 현재 이벤트 기간 조회
    - 투표 페이지에서 현재 이벤트가 투표 기간인지 확인하고,
      투표 전도 아니고 투표 중도 아니라면 alert 보여주고 event 메인
      페이지로 이동시킴

--- src/app/event/vote/EventVotePage.tsx
+++ src/app/event/vote/EventVotePage.tsx
@@ -1,26 +1,60 @@
 import { eventQueries } from "@/api/queries/eventQueries";
-import { EventPostCard } from "@/components/pages";
+import { EventPostCard, EventTimer } from "@/components/pages";
 import { ApiAnimalType } from "@/types/animal";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function EventVotePage() {
-  const { data } = useQuery({ ...eventQueries.eventPostList() });
+  const navigate = useNavigate();
+
+  const { data: eventPeriod } = useQuery({ ...eventQueries.eventPeriod() });
+  const { data: eventPostList, isPending: isEventPostListPending } = useQuery({
+    ...eventQueries.eventPostList(),
+  });
+
+  useEffect(() => {
+    if (eventPeriod?.status === null || eventPeriod?.status === "신청") {
+      alert("신청 기간이 아니다냥");
+      navigate("/event");
+    }
+  }, [eventPeriod, navigate]);
+
+  if (isEventPostListPending) {
+    return (
+      <div className="flex flex-col items-center p-5 text-2xl">
+        로딩중이다냥
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-row flex-wrap gap-3 p-2">
-      {data &&
-        data.map((post) => (
-          <EventPostCard
-            postId={post.postId}
-            postImageUrl={post.imageUrl}
-            userInfo={{
-              userId: 1,
-              profileImageUrl: post.profileImageUrl,
-              nickname: post.nickname,
-              animalType: ApiAnimalType.CAT,
-            }}
-            likeCount={post.likeCount}
+    <div className="flex flex-col gap-5 p-2">
+      {eventPeriod?.status === "투표중" && <div>순위</div>}
+      {eventPeriod?.status === "투표전" && (
+        <div className="flex flex-row items-center justify-center">
+          <EventTimer
+            title="투표까지 남은 시간"
+            endTimestamp={new Date(eventPeriod.time)}
           />
-        ))}
+        </div>
+      )}
+      <div className="flex flex-row flex-wrap gap-3 items-center justify-center">
+        {eventPostList &&
+          eventPostList.map((post) => (
+            <EventPostCard
+              postId={post.postId}
+              postImageUrl={post.imageUrl}
+              userInfo={{
+                userId: 1,
+                profileImageUrl: post.profileImageUrl,
+                nickname: post.nickname,
+                animalType: ApiAnimalType.CAT,
+              }}
+              likeCount={post.likeCount}
+            />
+          ))}
+      </div>
     </div>
   );
 }
