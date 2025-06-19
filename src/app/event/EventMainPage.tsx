6c83e580c7e30c90d163c43d0ddae8db1e2ccda2
jiyeeeah <sije3757@snu.ac.kr>
2025-06-19 14:01:49 +0900

    feat: 이벤트 페이지에서 각종 페이지로 이동하는 로직 구현

--- src/app/event/EventMainPage.tsx
+++ src/app/event/EventMainPage.tsx
@@ -3,15 +3,29 @@ import { IEventPeriodResponse } from "@/api/types/event";
 import { EventHistoryCard, EventTimer } from "@/components/pages";
 import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
-function renderBanner({ data }: { data: IEventPeriodResponse }) {
+function renderBanner({
+  data,
+  navigate,
+}: {
+  data: IEventPeriodResponse;
+  navigate: NavigateFunction;
+}) {
   switch (data.status) {
     case "신청":
       return (
         <EventTimer
           title="신청까지 남은 시간"
           endTimestamp={new Date(data.time)}
-          button={<Button variant="primarySolid">♤ 신청하러 가기 ♤</Button>}
+          button={
+            <Button
+              variant="primarySolid"
+              onClick={() => navigate("/event/apply")}
+            >
+              ♤ 신청하러 가기 ♤
+            </Button>
+          }
         />
       );
     case "투표전":
@@ -20,7 +34,14 @@ function renderBanner({ data }: { data: IEventPeriodResponse }) {
           title="투표까지 남은 시간"
           endTimestamp={new Date(data.time)}
           button={
-            <Button variant="secondarySolid">♧ 신청한 사진 보러 가기 ♧</Button>
+            <Button
+              variant="secondarySolid"
+              onClick={() => {
+                navigate("/event/poll");
+              }}
+            >
+              ♧ 신청한 사진 보러 가기 ♧
+            </Button>
           }
         />
       );
@@ -29,7 +50,14 @@ function renderBanner({ data }: { data: IEventPeriodResponse }) {
         <EventTimer
           title="투표 마감까지 남은 시간"
           endTimestamp={new Date(data.time)}
-          button={<Button variant="secondarySolid">♧ 투표하러 가기 ♧</Button>}
+          button={
+            <Button
+              variant="secondarySolid"
+              onClick={() => navigate("/event/poll")}
+            >
+              ♧ 투표하러 가기 ♧
+            </Button>
+          }
         />
       );
     default:
@@ -38,12 +66,13 @@ function renderBanner({ data }: { data: IEventPeriodResponse }) {
 }
 
 export default function EventMainPage() {
+  const navigate = useNavigate();
   const { data: eventPeriod } = useQuery({ ...eventQueries.eventPeriod() });
 
   // TODO: 데이터 연결
   return (
     <div className="p-5 flex flex-col gap-5">
-      {eventPeriod && renderBanner({ data: eventPeriod })}
+      {eventPeriod && renderBanner({ data: eventPeriod, navigate })}
       <EventHistoryCard
         title="제 5회 미스코리냥"
         subject="꿀잠 모먼트"
