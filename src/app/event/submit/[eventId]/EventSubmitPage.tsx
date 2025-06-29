4678fb8bf7b733f279a99223d0099a0a79d677f3
jiyeeeah <sije3757@snu.ac.kr>
2025-06-29 15:29:50 +0900

    style: 이벤트 신청 페이지 반응형 추가

--- src/app/event/submit/[eventId]/EventSubmitPage.tsx
+++ src/app/event/submit/[eventId]/EventSubmitPage.tsx
@@ -38,14 +38,14 @@ export default function EventSubmitPage() {
   }, [hasSubmittedData, navigate]);
 
   return (
-    <div className="w-full px-6 flex flex-col gap-5 items-center">
+    <div className="w-full px-6 pb-2 flex flex-col gap-3 items-center">
       <div className="flex flex-col gap-2 items-center mt-5">
         <p className="text-2xl md:text-4xl">
           ¢ 제 {eventId}회 미스코리냥 신청 ♧
         </p>
-        <p className="text-2xl">주제 : {topicData?.topic}</p>
+        <p className="text-xl md:text-2xl">주제 : {topicData?.topic}</p>
       </div>
-      <div className="flex flex-col gap-0 items-start text-base">
+      <div className="flex flex-col gap-0 items-start text-sm md:text-base">
         <p>• 한번 사진 제출하면 바꿀 수 없다냥</p>
         <p>• 사진은 위 아래나 양옆이 잘릴 수도 있다냥</p>
       </div>
