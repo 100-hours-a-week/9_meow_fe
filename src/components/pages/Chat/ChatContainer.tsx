2d8b366e503b4a491c339f7dd1b63cf9eadcf8a6
jiyeeeah <sije3757@snu.ac.kr>
2025-07-07 14:35:14 +0900

    feat: ChatContainer 컴포넌트 구현
    - ChatMessage와 ChatInput을 담고 있는 Container 컴포넌트 구현
    - Container가 스크롤 되도록 함 (바깥쪽 X)
    - TODO : 무한 스크롤로 채팅 메시지 불러오도록

---
+++ src/components/pages/Chat/ChatContainer.tsx
@@ -0,0 +1,23 @@
+import { ApiAnimalType } from "@/types/animal";
+import ChatMessage from "./ChatMessage";
+import ChatInput from "./ChatInput";
+
+export default function ChatContainer() {
+  return (
+    <div className="w-full h-full bg-foreground/10 rounded-xl gap-3 p-3 overflow-y-auto">
+      <div className="w-full flex flex-col-reverse items-center justify-start pb-25">
+        {/* TODO: 메시지 불러오는 로직 추가 */}
+        <ChatMessage
+          userId={1}
+          profileImageUrl="/logo.svg"
+          nickname="미야옹"
+          align="right"
+          message="안녕하세요! 미야옹입니다!"
+          animalType={ApiAnimalType.CAT}
+          createdAt="2025-01-01"
+        />
+      </div>
+      <ChatInput />
+    </div>
+  );
+}
