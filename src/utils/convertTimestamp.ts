b222d811aee546590a943a15a5582845441132f3
jiyeeeah <sije3757@snu.ac.kr>
2025-07-16 14:43:25 +0900

    fix: -1분전 안나오도록
    - 1분보다 적을 때는 방금 전이라고 뜨도록

--- src/utils/convertTimestamp.ts
+++ src/utils/convertTimestamp.ts
@@ -5,6 +5,10 @@ export const convertTimestamp = (timestamp: Date): string => {
   const diffInHours = Math.floor(diffInMs / (1000 * 60 * 60));
   const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 
+  if (diffInMinutes < 1) {
+    return "방금 전";
+  }
+
   if (diffInMinutes < 60) {
     return `${diffInMinutes}분 전`;
   }
