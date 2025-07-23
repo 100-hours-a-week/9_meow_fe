23e1e92bbfcc955a07477607f0f0e37954573caf
jiyeeeah <sije3757@snu.ac.kr>
2025-07-23 09:22:09 +0900

    fix: 배경 이미지 tailwind 설정 말고 picture 태그 활용

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -4,7 +4,11 @@ export default function Background({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-dvh overflow-hidden bg-[url(./background.png)] bg-cover bg-bottom">
+    <div className="flex h-dvh overflow-hidden bg-gradient-to-br from-rose-100 to-yellow-100">
+      <picture className="w-full h-full fixed top-0 left-0">
+        <source srcSet="/background.webp" media="(min-width: 768px)" />
+        <img src="/background.png" alt="background" className="w-full h-full" />
+      </picture>
       <div className="bg-white/50 fixed top-0 left-0 w-full h-full" />
       {children}
     </div>
